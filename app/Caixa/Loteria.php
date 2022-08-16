98222ac28d918e97ae2f339cbbc3197062c57ea4
daniel souza <workdndsza@gmail.com>
2022-08-16 18:22:56 -0300

    fazendo a consulta com o curl

--- app/Caixa/Loteria.php
+++ app/Caixa/Loteria.php
@@ -9,9 +9,28 @@ class Loteria {
 
   // Metódo responsavel por obter os resultados da loteria da caixa
   public static function consultarResultado($loteria){
-    echo "<pre>";
-    print_r($loteria);
-    echo "</pre>";
-  }
 
+    //endpoint completo
+    $endpoint = self::URL_BASE.'/'.$loteria;
+    
+    // inicia o cURL
+    $curl = curl_init();
+
+    //configura o curl
+    curl_setopt_array($curl, [
+      CURLOPT_URL => $endpoint,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_CUSTOMREQUEST => 'GET'
+    ]);
+
+    // executa o cURL
+    $response = curl_exec($curl);
+
+    // fecha o cURL
+    curl_close($curl);
+
+    echo "<pre>";
+    print_r($response);
+    echo "</pre>"; exit;
+  }
 }
